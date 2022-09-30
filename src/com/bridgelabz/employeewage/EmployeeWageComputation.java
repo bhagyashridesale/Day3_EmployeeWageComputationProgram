4551d84cc6cb901c820c37ef466cc7c7f9d1b2b4
bhagyashridesale <112363604+bhagyashridesale@users.noreply.github.com>
2022-09-30 14:46:08 +0530

    Added: Part Time Employee

--- src/com/bridgelabz/employeewage/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewage/EmployeeWageComputation.java
@@ -2,20 +2,25 @@ package com.bridgelabz.employeewage;
 
 public class EmployeeWageComputation {
     static final int WAGE_PER_HOUR = 20;
-    static final int FULL_DAY_HOUR = 8;
+    static final int FULL_DAY_HOUR = 12;
+
+    static final int PART_TIME_HOUR = 6;
 
     public static void main(String[] args) {
         int employeeAttendance=(int)Math.round(Math.random());
             System.out.println("Employee Attendance");
         int dailyWage = 0;
-            if (employeeAttendance==1) {
-                System.out.println("Employee is Present");
+            if (employeeAttendance==2) {
+                System.out.println("Employee is Full Time");
                 dailyWage= WAGE_PER_HOUR*FULL_DAY_HOUR;
             }
-            else{
-                System.out.print("Employee is Absent");
+            else if (employeeAttendance==1){
+                System.out.print("Employee is Part Time");
             }
-        System.out.print("Daily Wage =>" +dailyWage);
+            else {
+                System.out.println("Employee is absent");
+            }
+            System.out.println(" Daily Wage =>" +dailyWage);
 
     }
     }
